d653e321db06440b9c91f543c6e65a58b9ed8c63
Andreas Reichel <andreas@manticore-projects.com>
2023-12-12 16:28:20 +0700

    feat: Pivot Tables from ResultSet

--- src/main/java/com/manticore/jdbc/MJdbcTools.java
+++ src/main/java/com/manticore/jdbc/MJdbcTools.java
@@ -31,15 +31,26 @@ import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.text.Format;
+import java.text.NumberFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Formatter;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class MJdbcTools {
     public static final DateTimeFormatter SQL_DATE_FORMAT =
@@ -222,5 +233,183 @@ public class MJdbcTools {
         statement.accept(statementDeParser);
 
         return builder.toString();
+    }
+
+    public enum AggregateFunction {
+        SUM
+        , COUNT
+    }
+
+    private static TreeMap<Object, Object> emptyMapFromKeys(TreeSet<Object> keys) {
+        TreeMap<Object, Object> map = new TreeMap<>();
+        for (Object k:keys) {
+            map.put(k, null);
+        }
+        return map;
+    }
+
+    private static class ObjectArrayKey implements Comparable<ObjectArrayKey> {
+        private final Comparable[] keys;
+
+        public ObjectArrayKey(Object[] keys) {
+            this.keys = new Comparable[keys.length];
+            for (int i=0; i<keys.length; i++) {
+                this.keys[i] = (Comparable<?>) keys[i];
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ObjectArrayKey that = (ObjectArrayKey) o;
+
+            return Arrays.deepEquals(keys, that.keys);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(keys);
+        }
+
+        @Override
+        public int compareTo(ObjectArrayKey other) {
+            // Assuming both arrays have the same length
+            for (int i = 0; i < keys.length; i++) {
+                int elementComparison = keys[i].compareTo(other.keys[i]);
+                if (elementComparison != 0) {
+                    return elementComparison;
+                }
+            }
+
+            // If all elements are equal, the arrays are considered equal
+            return 0;
+        }
+    }
+
+    /**
+     * The getPivotFromQuery function takes a ResultSet and converts it into a pivot table.
+     * The function is designed to create columns for each key of the Category Column and to aggregate the values
+     * of the Aggregate Column for each Category.
+     *
+     * @param rs The ResultSet holding the source data with the category values in rows
+     * @param function Determine what type of aggregate function to use (SUM, COUNT, ...)
+     * @param categoryColumnName Identify the column that will be transformed into separate Value Columns
+     * @param categoryFormat Format the key values into column labels
+     * @param aggregateColumnName Specify the column name of the aggregate value
+     *
+     * @return A 2-dimensional array holding the Column Names and the Data
+     *
+     */
+    public static Object[][] getPivotFromQuery(ResultSet rs, AggregateFunction function, String categoryColumnName, String aggregateColumnName, Format categoryFormat) throws SQLException {
+        ArrayList<String> columnNames = new ArrayList<>();
+        int categoryColumnIndex = -1;
+        Class<?> categoryClass = null;
+        int aggregateColumnIndex = -1;
+        Class<?> aggregateClass = null;
+
+        ResultSetMetaData metaData = rs.getMetaData();
+        int columnCount = metaData.getColumnCount();
+        for (int i=1; i<=columnCount; i++) {
+            String columnName =  metaData.getColumnName(i);
+            if (columnName.equalsIgnoreCase(categoryColumnName)) {
+                try {
+                    categoryColumnIndex = i;
+                    categoryClass = Class.forName( metaData.getColumnClassName(i));
+                } catch (ClassNotFoundException ignore) {
+                }
+            } else if (columnName.equalsIgnoreCase(aggregateColumnName)) {
+                try {
+                    aggregateColumnIndex = i;
+                    aggregateClass = Class.forName( metaData.getColumnClassName(i));
+                } catch (ClassNotFoundException ignore) {
+                }
+            } else {
+                columnNames.add( columnName );
+            }
+        }
+
+        if (categoryClass==null || aggregateClass==null) {
+            throw new RuntimeException("Unable to determine the Aggregate or Category Classes.");
+        }
+
+
+        TreeSet<Object> keys = new TreeSet<>();
+        TreeMap<ObjectArrayKey, TreeMap<Object, Object>> data = new TreeMap<>();
+        ArrayList<Object> columnValues = new ArrayList<>();
+        Object keyValue;
+        Object aggregateValue;
+        while (rs.next()) {
+            columnValues.clear();
+            keyValue = null;
+            aggregateValue = null;
+
+            for (int i = 1; i<=columnCount; i++) {
+                Object value = rs.getObject(i);
+
+                if (i==categoryColumnIndex) {
+                    keyValue = value;
+                    // if we meet the key first time, we need to register it and extend the aggregated data maps
+                    if (!keys.contains(keyValue)) {
+                        keys.add(keyValue);
+                        for (TreeMap<Object, Object> aggregatedData: data.values()) {
+                            aggregatedData.put(keyValue, null);
+                        }
+                    }
+                } else if (i==aggregateColumnIndex) {
+                    aggregateValue = value;
+                } else {
+                    columnValues.add(rs.getObject(i));
+                }
+            }
+
+            ObjectArrayKey objectKey = new ObjectArrayKey(columnValues.toArray());
+
+            final TreeMap<Object, Object> rowData = data.getOrDefault( objectKey, emptyMapFromKeys(keys));
+            if (aggregateClass.equals(BigDecimal.class)) {
+                BigDecimal a = (BigDecimal) rowData.get(keyValue);
+                BigDecimal b = (BigDecimal) aggregateValue;
+
+                if (function == AggregateFunction.SUM) {
+                    if (a == null) {
+                        rowData.put(keyValue, b);
+                    }
+                    else if (b != null) {
+                        rowData.put(keyValue, a.add(b));
+                    }
+                } else {
+                    throw new UnsupportedOperationException("Only SUM is supported right now, sorry!");
+                }
+            } else {
+                throw new UnsupportedOperationException("Only BigDecimals are supported right now, sorry!");
+            }
+
+            data.put(objectKey, rowData);
+        }
+
+        // build the final data cube
+        for (Object k:keys) {
+            columnNames.add( categoryFormat!= null ? categoryFormat.format(k) : k.toString());
+        }
+
+        Object[][] resultData = new Object[data.size()][columnNames.size()];
+        int r = 0;
+        for (Map.Entry<ObjectArrayKey, TreeMap<Object, Object>> e : data.entrySet()) {
+            int c=0;
+            for (Object k: e.getKey().keys) {
+                resultData[r][c] = k;
+                c++;
+            }
+            for (Object k: keys) {
+                resultData[r][c] = e.getValue().get(k);
+                c++;
+            }
+            r++;
+        }
+
+        return new Object[][] {
+            columnNames.toArray(new String[columnNames.size()])
+            , resultData
+        };
     }
 }
